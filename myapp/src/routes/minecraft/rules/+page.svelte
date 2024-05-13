cd145a69435003e8f5828a4e3d41e41397bdbcb6
Stemmi20 <124707316+Stemmi20@users.noreply.github.com>
2024-05-13 11:06:08 +0200

    td to div

--- myapp/src/routes/minecraft/rules/+page.svelte
+++ myapp/src/routes/minecraft/rules/+page.svelte
@@ -1,23 +1,23 @@
 <div class="text-white">
     <h1 class="m-0">Rules</h1>
     <h5>If you bake one of these Rules you will get an timeout or permenatly Banned</h5>
-    <td class="m-10 p-10">
+    <div class="m-10 p-10">
         <h3>1.</h3><h5>Have fun</h5>
         <h3>2.</h3><h5>No Griefing</h5> 
         <h3>3.</h3><h5>No Stealing</h5>
-    </td>
-    <td>
+    </div>
+    <div>
         <h3>4.</h3><h5>Don´t kill</h5>
         <h3>5.</h3><h5>Don´t use xRay</h5>
         <h3>6.</h3><h5>Don´t scam</h5>
-    </td>
-    <td class="m-10 p-10">
+    </div>
+    <div class="m-10 p-10">
         <h3>7.</h3><h5>Don´t crash the Server on Purpos (e.g. Boad flood)</h5>
         <h3>8.</h3><h5>Don´t Destroy</h5>
         <h3>9.</h3><h5>Pay real money for Items</h5>
-    </td>
-    <td>    
+    </div>
+    <div>    
         <h3>10.</h3><h5>No offencive wording in chat</h5>
         <h3>11.</h3><h5>Appropriate Skin</h5>
-    </td>
+    </div>
 </div>
