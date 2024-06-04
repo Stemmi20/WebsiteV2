6a33790da159c84378d67d2011cbeeb054f4e7ea
Moritz Stemmler <stemmlermoritz300@gmail.com>
2024-06-04 19:25:07 +0200

    Register error message fix

--- myapp/src/routes/api/register/+server.ts
+++ myapp/src/routes/api/register/+server.ts
@@ -18,8 +18,9 @@ export const POST: RequestHandler = async (req) => {
 	const j = await req.request.json().catch(() => ({}));
 
 	const { password, username, firstname, lastname } = j;
+	console.log(j)
 	if (!password || !username) return error(400, 'No password or username provided');
-
+	if (!firstname.length || !lastname.length) return error(400, 'First or Last name are Missing');
 	if(!checkPassword(password)) return error(400, 'Password is insecure');
 
 	const exists = await DataBase.users.findFirst({ where: { username }, select: { username: true } });
