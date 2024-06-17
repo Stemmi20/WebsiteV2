dfd20cb0538c70bad1e766ac12aac4dc0ef26e24
Stemmi20 <124707316+Stemmi20@users.noreply.github.com>
2024-06-17 15:40:26 +0200

    background changes

--- myapp/src/routes/profile/+layout.svelte
+++ myapp/src/routes/profile/+layout.svelte
@@ -1,1 +1,1 @@
-<img class="fixed w-full h-full top-0 left-0 -z-1" src="profilebackground.png" alt="dcbackgound" />
+<!-- <img class="fixed w-full h-full top-0 left-0 -z-1" src="profilebackground.png" alt="dcbackgound" /> -->
