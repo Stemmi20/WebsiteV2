b167893c36d7d84a45f46a1b321bbd7e7dcc06b4
Stemmi20 <124707316+Stemmi20@users.noreply.github.com>
2024-07-03 12:59:46 +0200

    Fix when button on sidebar clicked close sidebar

--- myapp/src/lib/components/Buttons.svelte
+++ myapp/src/lib/components/Buttons.svelte
@@ -1,30 +1,58 @@
 <script lang="ts">
 	$: width = 0;
 </script>
+
 <div
 	id="menu"
-	class=" {width > 700 ? 'bg-red-7' : ''} p-1rem w-[100%] m-0 text-white flex {width > 700 ? 'flex-row' : 'flex-col'} justify-between items-center"
+	class=" {width > 700 ? 'bg-red-7' : ''} p-1rem w-[100%] m-0 text-white flex {width > 700
+		? 'flex-row'
+		: 'flex-col'} justify-between items-center"
 >
-	<div class="flex {width > 700 ? 'flex-row' : 'flex-col'} justify-center justify-start items-center gap-3 text-white">
-		<a href="/" class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge">Home</a>
-		<a href="/minecraft" class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge">Minecraft</a>
-		<a href="/discord" class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge">Discord</a>
-		<a href="/plugin" class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge">Plugin</a>
+	<div
+		class="flex {width > 700
+			? 'flex-row'
+			: 'flex-col'} justify-center justify-start items-center gap-3 text-white"
+	>
+		<a href="/" class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge" on:click={() => close()}
+			>Home</a
+		>
+		<a
+			href="/minecraft"
+			class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge"
+			on:click={() => close()}>Minecraft</a
+		>
+		<a
+			href="/discord"
+			class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge"
+			on:click={() => close()}>Discord</a
+		>
+		<a
+			href="/plugin"
+			class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge"
+			on:click={() => close()}>Plugin</a
+		>
 		<a
 			on:click={() => window.open('https://discord.gg/aV2RGPg7Yk')}
-			class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge">Support</a
+			class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge"
+			on:click={() => close()}>Support</a
 		>
 	</div>
-	<div class="flex {width > 700 ? 'flex-row' : 'flex-col mt-10 items-center'} gap-3 ">
-		<a href="/profile" class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge">Profile</a>
+	<div class="flex {width > 700 ? 'flex-row' : 'flex-col mt-10 items-center'} gap-3">
+		<a
+			href="/profile"
+			class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge"
+			on:click={() => close()}>Profile</a
+		>
 		<a
 			href="/login"
 			class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge"
 			on:click={() => fetch('/api/logout', { method: 'POST' }).then(() => window.location.reload())}
-			>Logout</a
+			on:click={() => close()}>Logout</a
 		>
-		<a href="/creaters&credits" class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge"
-			>Creaters & Credits</a
+		<a
+			href="/creaters&credits"
+			class="bg-[#0074f8] px-8px py-4px border-rd-8px enlarge"
+			on:click={() => close()}>Creaters & Credits</a
 		>
 	</div>
 </div>
