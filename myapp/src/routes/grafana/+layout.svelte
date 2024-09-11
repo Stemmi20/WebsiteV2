59abdfb62b0c1f2251b332347aae3b318696ad59
Stemmi20 <stemmlermoritz300@gmail.com>
2024-09-11 08:22:36 +0000

    Grafana implemention

---
+++ myapp/src/routes/grafana/+layout.svelte
@@ -0,0 +1,4 @@
+<img class="fixed w-full h-full top-0 left-0 -z-1" src="mcbackground.png" alt="dcbackgound" />
+<div class="min-h-100vh">
+	<slot />
+</div>
---
+++ myapp/src/routes/grafana/+page.svelte
@@ -0,0 +1,3 @@
+<div>
+    <img src="https://grafana.stemm1.org/d/rYdddlPWk/node-exporter-full?orgId=1&refresh=1m&from=1725956416520&to=1726042816520" alt="Grfana Dashboard">
+</div>
